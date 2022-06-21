cb23e56c5349148c5ee21d84d4b88293771041a8
EBSmash <53373788+EBSmash@users.noreply.github.com>
2022-06-21 10:03:57 -0400

    Done

--- src/main/kotlin/com/lambda/ExamplePlugin.kt
+++ src/main/kotlin/com/lambda/ExamplePlugin.kt
@@ -3,9 +3,7 @@ package com.lambda
 import com.lambda.client.LambdaMod
 import com.lambda.client.plugin.api.Plugin
 import com.lambda.client.util.threads.BackgroundJob
-import com.lambda.commands.ExampleCommand
 import com.lambda.huds.ExampleLabelHud
-import com.lambda.managers.ExampleManager
 import com.lambda.modules.ExampleModule
 
 internal object ExamplePlugin : Plugin() {
@@ -13,11 +11,7 @@ internal object ExamplePlugin : Plugin() {
     override fun onLoad() {
         // Load any modules, commands, or HUD elements here
         modules.add(ExampleModule)
-        commands.add(ExampleCommand)
         hudElements.add(ExampleLabelHud)
-        managers.add(ExampleManager)
-
-        bgJobs.add(BackgroundJob("ExampleJob", 10000L) { LambdaMod.LOG.info("Hello its me the BackgroundJob of your example plugin.") })
     }
 
     override fun onUnload() {
--- src/main/kotlin/com/lambda/huds/ExampleLabelHud.kt
+++ src/main/kotlin/com/lambda/huds/ExampleLabelHud.kt
@@ -5,16 +5,13 @@ import com.lambda.client.event.SafeClientEvent
 import com.lambda.client.plugin.api.PluginLabelHud
 
 internal object ExampleLabelHud : PluginLabelHud(
-    name = "ExampleLabelHud",
+    name = "FarmingHud",
     category = Category.MISC,
-    description = "Simple hud example",
+    description = "Simple hud to show what the farm bot is doing",
     pluginMain = ExamplePlugin
 ) {
-    private val prefix by setting("Prefix", "Hello")
-    private val suffix by setting("Suffix", "World")
 
     override fun SafeClientEvent.updateText() {
-        displayText.add(prefix, primaryColor)
-        displayText.add(suffix, secondaryColor)
+
     }
 }
